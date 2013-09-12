1601e53b7fdb437ccb6f9ea0eade3327103fc4ba
Prem kumar <Prem@Prems-MacBook-Pro-2.local>
2013-09-12 15:56:46 +0530

    added 4 more NSDate utilities

--- ios-utilities/DateUtility/NSDate+Utility.h
+++ ios-utilities/DateUtility/NSDate+Utility.h
@@ -10,6 +10,39 @@
 
 @interface NSDate (Utility)
 
+/**
+ It will calculate the time difference between self and the end date passed 
+ @param endDate, the end date to calculate the difference.
+ @return NSString, returns the time difference as NSString.
+ */
 -(NSString *)calculateTimeDifferenceBetweenDates:(NSDate *)endDate;
+
+/**
+ It will check if 'date' is older or equal to the self.date
+ @param date, reference date.
+ @return BOOL, Yes if condition passes.
+ */
+-(BOOL) isLaterThanOrEqualTo:(NSDate*)date;
+
+/**
+ It will check if 'date' is earlier or equal to the self.date
+ @param date, reference date.
+ @return BOOL, Yes if condition passes.
+ */
+-(BOOL) isEarlierThanOrEqualTo:(NSDate*)date;
+
+/**
+ It will check if 'date' is older that the self.date
+ @param date, reference date.
+ @return BOOL, Yes if condition passes.
+ */
+-(BOOL) isLaterThan:(NSDate*)date;
+
+/**
+ It will check if 'date' is older that the self.date
+ @param date, reference date.
+ @return BOOL, Yes if condition passes.
+ */
+-(BOOL) isEarlierThan:(NSDate*)date;
 
 @end
--- ios-utilities/DateUtility/NSDate+Utility.m
+++ ios-utilities/DateUtility/NSDate+Utility.m
@@ -34,4 +34,19 @@
     return timeString;
 }
 
+-(BOOL) isLaterThanOrEqualTo:(NSDate*)date {
+    return !([self compare:date] == NSOrderedAscending);
+}
+
+-(BOOL) isEarlierThanOrEqualTo:(NSDate*)date {
+    return !([self compare:date] == NSOrderedDescending);
+}
+-(BOOL) isLaterThan:(NSDate*)date {
+    return ([self compare:date] == NSOrderedDescending);
+    
+}
+-(BOOL) isEarlierThan:(NSDate*)date {
+    return ([self compare:date] == NSOrderedAscending);
+}
+
 @end
