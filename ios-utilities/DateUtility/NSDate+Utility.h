cf4d605468aa28f8ffa979363cc7f009f7777eb8
Prem kumar <Prem@Prems-MacBook-Pro-2.local>
2013-09-12 15:38:56 +0530

    Added calculateTimeDifferenceBetweenDates NSDate utility

---
+++ ios-utilities/DateUtility/NSDate+Utility.h
@@ -0,0 +1,15 @@
+//
+//  NSDate+Utility.h
+//  ios-utilities
+//
+//  Created by Prem kumar on 12/09/13.
+//  Copyright (c) 2013 freakApps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (Utility)
+
+-(NSString *)calculateTimeDifferenceBetweenDates:(NSDate *)endDate;
+
+@end
---
+++ ios-utilities/DateUtility/NSDate+Utility.m
@@ -0,0 +1,37 @@
+//
+//  NSDate+Utility.m
+//  ios-utilities
+//
+//  Created by Prem kumar on 12/09/13.
+//  Copyright (c) 2013 freakApps. All rights reserved.
+//
+
+#import "NSDate+Utility.h"
+
+@implementation NSDate (Utility)
+
+-(NSString *)calculateTimeDifferenceBetweenDates:(NSDate *)endDate
+{
+    NSTimeInterval timeIntervale  = [endDate timeIntervalSinceDate:self];
+    
+    int minutes = floor(timeIntervale/60);
+    int seconds = trunc(timeIntervale - minutes * 60);
+    int hours = minutes / 60;
+    NSString *timeString =@"";
+    NSString *formatString=@"";
+    if(hours > 0){
+        formatString=hours==1?@"%d hour":@"%d hours";
+        timeString = [timeString stringByAppendingString:[NSString stringWithFormat:formatString,hours]];
+    }
+    if(minutes > 0 || hours > 0 ){
+        formatString=minutes==1?@" %d minute":@" %d minutes";
+        timeString = [timeString stringByAppendingString:[NSString stringWithFormat:formatString,minutes]];
+    }
+    if(seconds >= 0 || hours > 0 || minutes > 0){
+        formatString=seconds==1?@" %d second":@" %d seconds";
+        timeString  = [timeString stringByAppendingString:[NSString stringWithFormat:formatString,seconds]];
+    }
+    return timeString;
+}
+
+@end
